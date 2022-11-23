ca0b52837a7f4d294bce66117a315bcbaf7961ff
Vadim Filonov <vafilo@ttu.ee>
2022-11-23 21:38:07 +0200

    created project

---
+++ iaib_pizza/pizza/static/pizza/readme.txt
@@ -0,0 +1,6 @@
+Do in menu/images directory 3 new directories:
+* pizza_images
+* burger_images
+* drinks_images
+
+And in each directory upload your images there
---
+++ iaib_pizza/pizza/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from pizza.menu_storage.pizza_menu import pizzas
+from pizza.menu_storage.burger_menu import burgers
+from pizza.menu_storage.drinks_menu import drinks
+
+
+def index(request):
+    return render(request, 'pizza/index.html')
+
+def pizza_menu(request):
+    return render(request, 'pizza/menu/pizza.html', {'pizzas': pizzas, 'title': 'Pizza menu'})
+
+def burger_menu(request):
+    return render(request, 'pizza/menu/burger.html', {'burgers': burgers, 'title': 'Burger menu'})
+
+def drinks_menu(request):
+    return render(request, 'pizza/menu/drinks.html', {'drinks': drinks, 'title': 'Drinks menu'})
+
+def cart(request):
+    return render(request, 'pizza/cart.html', {'title': 'Cart'})
